ec7a3c4371bb26e004daa7f223344d8b2d6a3a7b
Kevin Gutierrez <kevin.gutierrez@etu.univ-lyon1.fr>
2016-10-13 16:38:18 +0200

    Completed drawable templates of Circle and Rectangle

--- src/data/Circle.java
+++ src/data/Circle.java
@@ -12,18 +12,19 @@ import java.awt.Graphics;
 import view.CanvasPanel;
 
 public class Circle implements IDrawableSVG {
+    
 	private Vector2f m_position = new Vector2f(0,0);
 	private float m_radius = 0;
-    private Color m_color = Color.BLACK;
+        private Color m_color = Color.BLACK;
 
 	public void setPosition(Vector2f position) {
 		m_position = position;
 	}
-	public void setPosition(int x, int y) {
+	public void setPosition(float x, float y) {
 		m_position.x = x;
 		m_position.y = y;
 	}
-	public void setRadius(int radius) {
+	public void setRadius(float radius) {
 		m_radius = radius;
 	}
 	public void setColor(Color color) {
@@ -35,9 +36,9 @@ public class Circle implements IDrawableSVG {
 		g.setColor(m_color);
 		float zoom = CanvasPanel.zoom;
 		int x, y, r;
-		x = (int)(m_position.x * zoom);
-		y = (int)(m_position.y * zoom);
+		x = (int)((m_position.x - m_radius) * zoom);
+		y = (int)((m_position.y - m_radius) * zoom);
 		r = (int)(m_radius * zoom);
-		g.drawOval(x, y, r, r);
+		g.drawOval(x, y, r*2, r*2);
 	}
 }
--- src/data/Rectangle.java
+++ src/data/Rectangle.java
@@ -12,19 +12,38 @@ import view.CanvasPanel;
 
 public class Rectangle implements IDrawableSVG {
 
-	private Vector2f m_position = new Vector2f(0,0);
-	private Vector2f m_size = new Vector2f(0,0);
-	private Color m_color = Color.BLACK;
+    private Vector2f m_position = new Vector2f(0, 0);
+    private Vector2f m_size = new Vector2f(0, 0);
+    private Color m_color = Color.BLACK;
 
-	@Override
-	public void render(Graphics g) {
-		g.setColor(m_color);
-		float zoom = CanvasPanel.zoom;
-		int x, y, w, h;
-		x = (int)(m_position.x * zoom);
-		y = (int)(m_position.y * zoom);
-		w = (int)(m_size.x * zoom);
-		h = (int)(m_size.y * zoom);
-		g.drawRect(x, y, w, h);
-	}
+    public void setPosition(Vector2f position) {
+        m_position = position;
+    }
+
+    public void setPosition(float x, float y) {
+        m_position.x = x;
+        m_position.y = y;
+    }
+    
+    public void setSize(Vector2f size) {
+        m_size = size;
+    }
+
+    public void setSize(float width, float height) {
+        m_size.x = width;
+        m_size.y = height;
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.setColor(m_color);
+        float zoom = CanvasPanel.zoom;
+        int x, y, w, h;
+        x = (int) (m_position.x * zoom);
+        y = (int) (m_position.y * zoom);
+        w = (int) (m_size.x * zoom);
+        h = (int) (m_size.y * zoom);
+        g.drawRect(x, y, w, h);
+    }
+
 }
