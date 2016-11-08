b205536855f702a416904b61e55816d59a671c7a
Dare <zeratul_du_38@hotmail.fr>
2016-11-08 10:22:13 +0100

    Better parser (keep elements order)

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -37,124 +37,21 @@ public class Parser {
 		if (doc != null) {
 			SVG svg = new SVG();
 
-			// Parsing circles
-			NodeList nodeList = doc.getElementsByTagName("circle");
-			if (nodeList.getLength() > 0) {
-				for (int i = 0; i < nodeList.getLength(); i++) {
-					Circle circle = new Circle();
-					Node node = nodeList.item(i);
-
-					if (node.getAttributes().getNamedItem("style") != null) {
-						String style = node.getAttributes().getNamedItem("style").getNodeValue();
-						for (String s : style.split(";")) {
-							if (s.contains("stroke:")) {
-								circle.setStrokeColor(Color.decode(s.replace("stroke:", "")));
-							}
-							if (s.contains("fill:")) {
-								circle.setFillColor(Color.decode(s.replace("fill:", "")));
-							}
-						}
-					}
-
-					float cx = Float.parseFloat(node.getAttributes().getNamedItem("cx").getNodeValue());
-					float cy = Float.parseFloat(node.getAttributes().getNamedItem("cy").getNodeValue());
-					float r = Float.parseFloat(node.getAttributes().getNamedItem("r").getNodeValue());
-					circle.setPosition(cx, cy);
-					circle.setRadius(r);
-
-					svg.getDrawableList().add(circle);
-				}
-			}
-
-			// Parsing rectangle
-			nodeList = doc.getElementsByTagName("rect");
-			if (nodeList.getLength() > 0) {
-				for (int i = 0; i < nodeList.getLength(); i++) {
-					Rectangle rect = new Rectangle();
-					Node node = nodeList.item(i);
-
-					if (node.getAttributes().getNamedItem("style") != null) {
-						String style = node.getAttributes().getNamedItem("style").getNodeValue();
-						for (String s : style.split(";")) {
-							if (s.contains("stroke:")) {
-								rect.setStrokeColor(Color.decode(s.replace("stroke:", "")));
-							}
-							if (s.contains("fill:")) {
-								rect.setFillColor(Color.decode(s.replace("fill:", "")));
-							}
-						}
-					}
-
-					float x = Float.parseFloat(node.getAttributes().getNamedItem("x").getNodeValue());
-					float y = Float.parseFloat(node.getAttributes().getNamedItem("y").getNodeValue());
-					float width = Float.parseFloat(node.getAttributes().getNamedItem("width").getNodeValue());
-					float height = Float.parseFloat(node.getAttributes().getNamedItem("height").getNodeValue());
-					rect.setPosition(x, y);
-					rect.setSize(width, height);
-
-					svg.getDrawableList().add(rect);
-				}
-			}
-
-			// Parsing paths
-			nodeList = doc.getElementsByTagName("path");
-
+			// Parsing nodes
+			NodeList nodeList = doc.getFirstChild().getChildNodes();
 			for (int i = 0; i < nodeList.getLength(); i++) {
-
-				Path path = new Path();
-
 				Node node = nodeList.item(i);
-
-				// Parsing style
-				if (node.getAttributes().getNamedItem("style") != null) {
-					String style = node.getAttributes().getNamedItem("style").getNodeValue();
-					for (String s : style.split(";")) {
-						if (s.contains("stroke:")) {
-							path.setStrokeColor(Color.decode(s.replace("stroke:", "")));
-						}
-						if (s.contains("stroke-width:")) {
-							path.setStrokeWidth(Float.parseFloat(s.replace("stroke-width:", "")));
-						}
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+					if (node.getNodeName().equals("circle")) {
+						loadNodeCircle(node, svg);
+					}
+					if (node.getNodeName().equals("rect")) {
+						loadNodeRect(node, svg);
+					}
+					if (node.getNodeName().equals("path")) {
+						loadNodePath(node, svg);
 					}
 				}
-
-				// String to be scanned to find the pattern.
-				String d = node.getAttributes().getNamedItem("d").getNodeValue();
-
-				String pattern = "[a-z][^a-z]*";
-
-				// Create a Pattern object.
-				Pattern r = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
-
-				// Now create matcher object.
-				Matcher m = r.matcher(d);
-				float previousX = 0f;
-				float previousY = 0f;
-
-				while (m.find()) {
-					String match = m.group(0);
-
-					if (match.startsWith("m") || match.startsWith("M")) {
-						String coord = match.replace("m ", "").replace("M ", "");
-						previousX = Float.parseFloat(coord.split(",")[0]);
-						previousY = Float.parseFloat(coord.split(",")[1]);
-					} else if (match.startsWith("c") || match.startsWith("C")) {
-						String coord = match.replace("c ", "").replace("C ", "");
-						Curve curve = new Curve();
-						for (String s : coord.split(" ")) {
-							if (curve.getStops().isEmpty()) {
-								curve.addStop(previousX, previousY);
-							}
-							previousX = Float.parseFloat(s.split(",")[0]);
-							previousY = Float.parseFloat(s.split(",")[1]);
-
-							curve.addStop(previousX, previousY);
-						}
-						path.addCurve(curve);
-					}
-				}
-
-				svg.getDrawableList().add(path);
 			}
 
 			return svg;
@@ -162,6 +59,110 @@ public class Parser {
 			return null;
 		}
 
+	}
+
+	public void loadNodeCircle(Node node, SVG svg) {
+		Circle circle = new Circle();
+
+		if (node.getAttributes().getNamedItem("style") != null) {
+			String style = node.getAttributes().getNamedItem("style").getNodeValue();
+			for (String s : style.split(";")) {
+				if (s.contains("stroke:")) {
+					circle.setStrokeColor(Color.decode(s.replace("stroke:", "")));
+				}
+				if (s.contains("fill:")) {
+					circle.setFillColor(Color.decode(s.replace("fill:", "")));
+				}
+			}
+		}
+
+		float cx = Float.parseFloat(node.getAttributes().getNamedItem("cx").getNodeValue());
+		float cy = Float.parseFloat(node.getAttributes().getNamedItem("cy").getNodeValue());
+		float r = Float.parseFloat(node.getAttributes().getNamedItem("r").getNodeValue());
+		circle.setPosition(cx, cy);
+		circle.setRadius(r);
+
+		svg.addDrawable(circle);
+	}
+
+	public void loadNodeRect(Node node, SVG svg) {
+		Rectangle rect = new Rectangle();
+
+		if (node.getAttributes().getNamedItem("style") != null) {
+			String style = node.getAttributes().getNamedItem("style").getNodeValue();
+			for (String s : style.split(";")) {
+				if (s.contains("stroke:")) {
+					rect.setStrokeColor(Color.decode(s.replace("stroke:", "")));
+				}
+				if (s.contains("fill:")) {
+					rect.setFillColor(Color.decode(s.replace("fill:", "")));
+				}
+			}
+		}
+
+		float x = Float.parseFloat(node.getAttributes().getNamedItem("x").getNodeValue());
+		float y = Float.parseFloat(node.getAttributes().getNamedItem("y").getNodeValue());
+		float width = Float.parseFloat(node.getAttributes().getNamedItem("width").getNodeValue());
+		float height = Float.parseFloat(node.getAttributes().getNamedItem("height").getNodeValue());
+		rect.setPosition(x, y);
+		rect.setSize(width, height);
+
+		svg.addDrawable(rect);
+	}
+
+	public void loadNodePath(Node node, SVG svg) {
+		Path path = new Path();
+
+		// Parsing style
+		if (node.getAttributes().getNamedItem("style") != null) {
+			String style = node.getAttributes().getNamedItem("style").getNodeValue();
+			for (String s : style.split(";")) {
+				if (s.contains("stroke:")) {
+					path.setStrokeColor(Color.decode(s.replace("stroke:", "")));
+				}
+				if (s.contains("stroke-width:")) {
+					path.setStrokeWidth(Float.parseFloat(s.replace("stroke-width:", "")));
+				}
+			}
+		}
+
+		// String to be scanned to find the pattern.
+		String d = node.getAttributes().getNamedItem("d").getNodeValue();
+
+		String pattern = "[a-z][^a-z]*";
+
+		// Create a Pattern object.
+		Pattern r = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+
+		// Now create matcher object.
+		Matcher m = r.matcher(d);
+		float previousX = 0f;
+		float previousY = 0f;
+
+		while (m.find()) {
+			String match = m.group(0);
+
+			if (match.startsWith("m") || match.startsWith("M")) {
+				String coord = match.replace("m ", "").replace("M ", "");
+				previousX = Float.parseFloat(coord.split(",")[0]);
+				previousY = Float.parseFloat(coord.split(",")[1]);
+			} else if (match.startsWith("c") || match.startsWith("C")) {
+				String coord = match.replace("c ", "").replace("C ", "");
+				Curve curve = new Curve();
+				for (String s : coord.split(" ")) {
+					if (curve.getStops().isEmpty()) {
+						curve.addStop(previousX, previousY);
+					}
+					previousX = Float.parseFloat(s.split(",")[0]);
+					previousY = Float.parseFloat(s.split(",")[1]);
+
+					curve.addStop(previousX, previousY);
+				}
+				path.addCurve(curve);
+			}
+		}
+
+		svg.addDrawable(path);
 	}
 
 }
